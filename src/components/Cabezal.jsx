f0060ec4c22f45016fedf7d7829d48121cbbf0d0
Enzo <91644783+aesaries@users.noreply.github.com>
2024-01-10 21:35:52 -0300

    Arreglando OTRA vez el logo de cabecera

--- src/components/Cabezal.jsx
+++ src/components/Cabezal.jsx
@@ -1,10 +1,11 @@
 import './Cabezal.css';
+import { logoPng } from "../src/images/logoaf.png";
 
 export const Cabezal = () => {
     
     return (
       <nav>
-        <img src="../src/images/logoaf.png" alt='LogoAf' className="logo"></img>
+        <img src= {logoPng} alt='LogoAf' className="logo"></img>
         <div className="barnav">
           <div>Agrotecnica Fueguina</div>
           
