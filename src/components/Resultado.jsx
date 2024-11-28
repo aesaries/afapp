20e1cb8bc956c9b7509065e5c7fc9962117ae028
Enzo <ecavsyh@gmail.com>
2024-11-27 22:38:14 -0300

    Agregando fecha de nacimiento al resultado de la busqueda

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,47 +1,48 @@
 import { useFormContext } from './FormContext';
 import "./Resultado.css"
-import  personalData from "../data/Personal.json"
+import personalData from "../data/Personal.json"
 
 const filtrarDatos = (campo, parametro) => {
-    return personalData.filter(item => {
-        if (parametro === "Legajo") {
-            return item.Legajo.startsWith(campo);
-        } else if (parametro === "Apellido") {
-            return item.Apellido.toLowerCase().includes(campo.toLowerCase());
-        }
-        // Agrega más casos según tus necesidades
-        return false;
-    });
+  return personalData.filter(item => {
+    if (parametro === "Legajo") {
+      return item.Legajo.startsWith(campo);
+    } else if (parametro === "Apellido") {
+      return item.Apellido.toLowerCase().includes(campo.toLowerCase());
+    }
+    // Agrega más casos según tus necesidades
+    return false;
+  });
 }
 
 export const Resultado = () => {
-    const { formData } = useFormContext();
-    const campo = formData.campo
-    const parametro = formData.parametro
+  const { formData } = useFormContext();
+  const campo = formData.campo
+  const parametro = formData.parametro
 
-    const resultados = filtrarDatos(campo, parametro);
+  const resultados = filtrarDatos(campo, parametro);
 
-    return (
-      <div className='resultadoContainer'>
-        {resultados.length > 0 ? (
-          resultados.map((item) => (
-            <div key={item.Legajo} className="card">
-              <h4>
-                {item.Apellido}, {item.Nombres}
-              </h4>
-              <p>Legajo: {item.Legajo}</p>
-              <p>Documento: {item.Documento}</p>
-              <p>Fecha Ingreso: {item.Alta}</p>
-              <p>Categoria: {item.Categoria}</p>
-              <p>Sector: {item.Sector}</p>    
-                  
-                  
-              {/* Agrega más campos según sea necesario */}
-            </div>
-          ))
-        ) : (
-          <p className='nada'>No se encontraron resultados</p>
-        )}
-      </div>
-    ); 
+  return (
+    <div className='resultadoContainer'>
+      {resultados.length > 0 ? (
+        resultados.map((item) => (
+          <div key={item.Legajo} className="card">
+            <h4>
+              {item.Apellido}, {item.Nombres}
+            </h4>
+            <p>Legajo: {item.Legajo}</p>
+            <p>Documento: {item.Documento}</p>
+            <p>Fecha Ingreso: {item.Alta}</p>
+            <p>Fecha Nac: {item.F_Nacimiento}</p>
+            <p>Categoria: {item.Categoria}</p>
+            <p>Sector: {item.Sector}</p>
+
+
+            {/* Agrega más campos según sea necesario */}
+          </div>
+        ))
+      ) : (
+        <p className='nada'>No se encontraron resultados</p>
+      )}
+    </div>
+  );
 }
