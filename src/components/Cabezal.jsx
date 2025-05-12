2b96e3c9adadc3a7131178a2a9e4ae7dce700bf1
Enzo <ecavsyh@gmail.com>
2025-05-12 20:30:19 -0300

    Actualizando BBDD JSon

--- src/components/Cabezal.jsx
+++ src/components/Cabezal.jsx
@@ -1,19 +1,22 @@
 import './Cabezal.css';
 
+const savedUser = localStorage.getItem("user");
+const userName = savedUser.userName;
+const pass = savedUser.password;
 
 export const Cabezal = () => {
-    
-    return (
-      <nav>
-        <img src= "/logoaf.png" alt='LogoAf' className="logo"></img>
-        <div className="barnav">
-          <h1>Agrotecnica Fueguina</h1>
-        </div>
-        <div>
-          
-        </div>
-      </nav>
-    );
+
+  return (
+    <nav>
+      <img src="/logoaf.png" alt='LogoAf' className="logo"></img>
+      <div className="barnav">
+        <h1>Agrotecnica Fueguina</h1>
+      </div>
+      <div>
+        <h5>{userName}</h5>
+      </div>
+    </nav>
+  );
 
 
 }
